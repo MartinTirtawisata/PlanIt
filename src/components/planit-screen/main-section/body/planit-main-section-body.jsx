2014ed92eabc69ffaf5cc5706b49e58a2c143d99
Martin Tirtawisata <martintirtawisata@Martins-MacBook-Pro.local>
2019-06-16 11:20:11 -0700

    added google=maps-react

--- src/components/planit-screen/main-section/body/planit-main-section-body.jsx
+++ src/components/planit-screen/main-section/body/planit-main-section-body.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import {Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
 import './planit-main-section-body';
 
-export default function PlanItMainSectionBody(){
-    return(
-        <div style={{border: '1px solid black', width:'1382px', height: '820px', margin: 'auto'}}>
-            Google Maps
-        </div>
-    )
+export class PlanItMainSectionBody extends React.Component {
+    render(){
+
+    const mapStyles = {
+        width: '100%',
+        height: '100%'
+    }
+        return(
+            <div style={{border: '1px solid black', width:'100%', height: 'auto', margin: 'auto'}}>
+                <Map 
+                    google={this.props.google}
+                    zoom={8}
+                    style={mapStyles}
+                    initialCenter={{ lat: 47.444, lng: -122.176}}    
+                >
+                    <Marker position={{lat: 48.00, lng: -122.00}} />
+                </Map>
+
+            </div>
+        )
+    }
 }
+
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyDCbKjvR3xSU6ODz37cp9YmRgb3pYMH0i8'
+})(PlanItMainSectionBody)
